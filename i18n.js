33a0d951a49b3ef8ce1604d32a25f37d4318699c
Chris Finn <chris@finn.com>
2021-01-26 14:21:28 -0500

    Add i18n.

---
+++ i18n.js
@@ -0,0 +1,8 @@
+module.exports = {
+  locales: ['en-US', 'pt-BR'],
+  defaultLocale: 'en-US',
+  pages: {
+    '*': ['common'],
+    '/': ['home'],
+  },
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import { useEffect, useState } from 'react'
+import useTranslation from 'next-translate/useTranslation'
+import Link from 'next/link'
 import { useTheme } from 'next-themes'
+import { useRouter } from 'next/router'
 
 export default function Home(): JSX.Element {
   const [isMounted, setIsMounted] = useState(false)
+  const router = useRouter()
+  const { locale, defaultLocale } = router
+  const { t } = useTranslation('home')
   const { theme, setTheme } = useTheme()
   useEffect(() => {
     setIsMounted(true)
@@ -16,14 +22,28 @@ export default function Home(): JSX.Element {
     <div className="bg-gray-200 dark:bg-gray-900 h-screen flex justify-center items-center">
       <div className="p-6 max-w-sm max-h-80 bg-white dark:bg-gray-500 rounded-xl shadow-md flex items-center space-x-4">
         <div className="flex flex-col justify-center items-center">
-          <div className="text-xl font-medium text-black dark:text-white">Home</div>
-          <p className="text-gray-500 dark:text-gray-300 mb-4">This is the home screen styled with Tailwind.</p>
+          <div className="text-xl font-medium text-black dark:text-white">{t('title')}</div>
+          <p className="text-gray-500 dark:text-gray-300 mb-4">{t('description')}</p>
+          <div className="mb-4 dark:text-white">
+            <p>{t('currentLocale', { locale })}</p>
+            <p>{t('defaultLocale', { defaultLocale })}</p>
+            <ul>
+              {router.locales.map((lcl) => (
+                <li key={lcl} className="text-blue-400">
+                  <Link href={router.asPath} locale={lcl}>
+                    {lcl}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <p>{t('currentTheme', { theme })}</p>
+          </div>
           <button
             type="button"
             className="p-2 border border-black dark:border-white dark:text-white"
             onClick={switchTheme}
           >
-            Change theme
+            {t('changeTheme')}
           </button>
         </div>
       </div>
